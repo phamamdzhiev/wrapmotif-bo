515916fe9bd9a0f7e2aafcc5b7c41d43ed0f295c
zoxidovuz <umidjonzoxidov96@gmail.com>
2022-05-19 15:20:30 +0500

    Fix

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (\App\Facades\Helper::getLightLogo())
-                <img src="{{ \App\Facades\Helper::getLightLogo() }}" class="logo" alt="Logo" style="width: auto">
+                <img src="{{ \App\Facades\Helper::getLightLogo() }}" class="logo" alt="Logo" style="width: 90%">
             @else
                 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Logo">
             @endif
