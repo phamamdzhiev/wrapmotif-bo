4cd56f8569bfbf3264feeb390dd065230d1f3ba8
Petar Hamamdzhiev <petar@condor-gaming.com>
2022-06-26 11:02:30 +0300

    chane

--- resources/js/Pages/Orders/Index.vue
+++ resources/js/Pages/Orders/Index.vue
@@ -53,7 +53,7 @@
                     <td class="px-6 py-4 whitespace-nowrap">{{ row.grandTotalFormatted }}</td>
                     <td class="px-6 py-4 whitespace-nowrap">{{ row.customerGrandTotalFormatted }}</td>
                     <td class="px-6 py-4 whitespace-nowrap">
-                        <strong>{{ (row.status) === 0 ? 'Pending' : 'Paid' }}</strong>
+                        <strong class="text-uppercase">{{ row.status }}</strong>
                     </td>
 
                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
